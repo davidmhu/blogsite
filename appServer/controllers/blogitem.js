148bbb31978e6be80e9634b9ca801aeb83594e3c
david hu <davidhu@davidtekiMacBook-Air.local>
2016-03-15 22:42:12 +0800

    show blog list

---
+++ appServer/controllers/blogitem.js
@@ -0,0 +1,56 @@
+var request = require('request');
+var apiOptions = {
+  server : "http://localhost:3100"//+process.env.PORT ;
+};
+console.log(apiOptions.server);
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = "https://blogsite.com";//need to modify
+}
+
+var _showError = function (req, res, status) {
+  var title, content;
+  if (status === 404) {
+    res.render('404', {
+      message: "Page not found",
+      error:"Cannot find blogs"
+    });
+  } else if (status === 500) {
+    title = "500, internal server error";
+    content = "How embarrassing. There's a problem with our server.";
+  } else {
+    title = status + ", something's gone wrong";
+    content = "Something, somewhere, has gone just a little bit wrong.";
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title : title,
+    content : content
+  });
+};
+
+var renderHomepage = function(req, res, responseBody){ 
+	res.render('blog-list', { 
+		blogs: responseBody 
+	});
+};
+
+module.exports.blogList = function(req, res){
+  var requestOptions, path;
+  path = "/api/blog/" ;
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "GET",
+    json : {}
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var data = body;
+      if (response.statusCode === 200) {console.log(data);        
+        renderHomepage(req,res,data);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
+    }
+  );
+};
--- appServer/routes/index.js
+++ appServer/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var ctrlBlogitem=require('../controllers/blogItem');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/blog',ctrlBlogitem.blogList);
 module.exports = router;

bd337798f0d3a845ad97f3b017eeeb9473f2acf0
david hu <davidhu@davidtekiMacBook-Air.local>
2016-05-20 22:32:22 +0800

    formatTime filter begin

---
+++ appClient/common/formatTime.filter.js
@@ -0,0 +1,14 @@
+(function () {
+
+  angular
+    .module('blogsiteApp')
+    .filter('formatTime', formatTime);
+
+  function formatTime () {
+    return function(time){
+      var numTime,unit;
+      //if (time && )
+    };
+  }
+
+})();

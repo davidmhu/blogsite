5fc6b0ef03db0318b095f2424e8154fd3e468f5d
david hu <davidhu@davidtekiMacBook-Air.local>
2016-05-31 22:40:04 +0800

    ueditor imgUpload begin

--- blogsiteMain.js
+++ blogsiteMain.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ueditor = require("ueditor");
 var uglifyJs = require("uglify-js");
 var fs = require('fs');
 if (!fs.existsSync(process.env.UPLOAD_DIR)) fs.mkdirSync(process.env.UPLOAD_DIR);
@@ -17,6 +18,7 @@ require('./api/models/db');
 require('./api/config/passport');
 
 var routes = require('./appServer/routes/index');
+//var ueditorRoutes=require('./appServer/routes/ueditor');
 //var users = require('./appServer/routes/users');
 var apiRoutes = require('./api/routes/index');
 var app = express();
@@ -89,7 +91,33 @@ app.use(function(req, res, next) {
 // routes
 app.use('/', routes);
 //app.use('/users', users);
+
 app.use('/api', apiRoutes);
+
+//app.use("/ueditor/",ueditorRoutes);
+app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+    // ueditor 客户发起上传图片请求
+    if (req.query.action === 'uploadimage') {
+        var foo = req.ueditor;
+        var date = new Date();
+        var imgname = req.ueditor.filename;
+
+        var img_url = '/images/ueditor/';
+        res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+    }
+    //  客户端发起图片列表请求
+    else if (req.query.action === 'listimage') {
+        var dir_url = '/images/ueditor/';
+        res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
+    }
+    // 客户端发起其它请求
+    else {
+        // console.log('config.json')
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/ueditor/nodejs/config.json');
+    }
+}));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Page Not Found');

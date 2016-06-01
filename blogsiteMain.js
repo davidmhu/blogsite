bac2e4b587b87b913bd8d37707af7168ecab3d94
davidmhu <davidmhu@outlook.com>
2016-06-01 17:30:25 +0800

    ueditor img Upload not yet

--- blogsiteMain.js
+++ blogsiteMain.js
@@ -68,7 +68,7 @@ fs.writeFile('public/js/blogsite.min.js', uglified.code, function (err){
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
 app.use(cookieParser());
 app.use(session({ secret: process.env.COOKIE_SECRET, resave:false,saveUninitialized:true,cookie: { maxAge: 900000}}));
@@ -95,26 +95,28 @@ app.use('/', routes);
 app.use('/api', apiRoutes);
 
 //app.use("/ueditor/",ueditorRoutes);
-app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+app.use("/ueditor/ue/", ueditor(path.join(__dirname, 'public'), function(req, res,next) {
     // ueditor 客户发起上传图片请求
-    if (req.query.action === 'uploadimage') {
+    console.log(req.query);
+    console.log(path.join(__dirname, 'public'));
+    if (req.query.action === 'uploadimage') {console.log('in uploadimage');
         var foo = req.ueditor;
         var date = new Date();
         var imgname = req.ueditor.filename;
 
-        var img_url = '/images/ueditor/';
+        var img_url = '/uploads';
         res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
     }
     //  客户端发起图片列表请求
     else if (req.query.action === 'listimage') {
-        var dir_url = '/images/ueditor/';
+        var dir_url = '/uploads';
         res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
     }
     // 客户端发起其它请求
     else {
-        // console.log('config.json')
+         console.log('config.json');
         res.setHeader('Content-Type', 'application/json');
-        res.redirect('/ueditor/nodejs/config.json');
+        res.redirect('/js/vendor/ueditor/nodejs/config.json');
     }
 }));
 
